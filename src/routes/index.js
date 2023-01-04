f4e97e99ece7113b4567b1143deb0b6e356811e9
Kang ChangRyong <rkdckdfyyd@naver.com>
2023-01-04 15:20:36 +0900

    feat: ftp-api

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const index = require("../controller");
 
 /* GET home page. */
 // 첫번째 param = URL 정의 || 두번째 param = 해당 URL에서 할 작업 → req: URL에 담긴 요청정보 res: 작업 수행 후 보내줄 정보
-router.get('/index', index.indexTest);
+router.get("/index", index.indexTest);
 
-router.get('/index/video',index.youtubeVideo)
+router.get("/index/video", index.youtubeVideo);
+
+router.get("/index/sendVideo", index.sendVideo);
 
 module.exports = router;
